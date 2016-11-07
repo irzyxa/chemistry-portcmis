c538bfc3f97f77dd632fa30350a6f3b94552394b
Alexandr Irza <irzyxa@gmail.com>
2016-11-07 14:06:31 +0200

    Incorrect set parameters link and type in function AddTypeLink

--- PortCMIS/binding/atompub/AtomPubUtils.cs
+++ PortCMIS/binding/atompub/AtomPubUtils.cs
@@ -1010,7 +1010,7 @@ namespace PortCMIS.Binding.AtomPub
         {
             if (KnownLinks.Contains(rel))
             {
-                typeLinkCache.Put(new string[] { link, repositoryId, id, rel }, type);
+                typeLinkCache.Put(new string[] { repositoryId, id, rel, type }, link);
             }
         }
 
